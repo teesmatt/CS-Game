89dab1b2b165f66b34f7922d9145318325b56f59
Skytown19 <sethstevenson2005@gmail.com>
2016-04-04 19:10:39 -0300

    I did it!

--- CSRPG/src/csRpg/Inventory.java
+++ CSRPG/src/csRpg/Inventory.java
@@ -1,5 +1,45 @@
 package csRpg;
 
-public class Inventory {
+import java.util.ArrayList;
 
+public class Inventory {
+	private ArrayList<Item> items;
+
+	public Inventory()
+	{
+		items = new ArrayList<Item>();
+	}
+
+	public void addItem(Item item)
+	{
+		items.add(item);
+	}
+	
+	public void removeItem(Item item)
+	{
+		if(items.isEmpty())
+			return;
+		else
+			items.remove(item);
+	}
+	
+	public int numItems()
+	{
+		return items.size();
+	}
+	
+	public Item getItem(int n)
+	{
+		return items.get(n);
+	}
+
+	public ArrayList<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<Item> items) {
+		this.items = items;
+	}
+	
+	
 }
