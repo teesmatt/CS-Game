021907546f0455a9eb7d397e8a4b0934ee42194a
adam fowler <115805f@acaidiau.ca>
2016-04-06 13:13:24 -0300

    added data to game over window and score to buisness game

--- CSRPG/src/csRpg/GameOver.java
+++ CSRPG/src/csRpg/GameOver.java
@@ -11,6 +11,8 @@ import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 
 public class GameOver extends BasicGameState{
 
@@ -20,6 +22,9 @@ public class GameOver extends BasicGameState{
     private Image background;
     
     private Rectangle box;
+    
+    private Button back;
+	private StateBasedGame game;
     
 	public GameOver() {
 		// TODO Auto-generated constructor stub
@@ -37,11 +42,16 @@ public class GameOver extends BasicGameState{
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		// TODO Auto-generated method stub
+
+		this.game = game;
 		
 		// loads the image for the background of the menu
 		this.background = new Image("/assets/Background.jpeg");
 		
-		this.box = new Rectangle(50,50,container.getWidth()-100,container.getHeight()-100);
+		this.box = new Rectangle(50,50,600,container.getHeight()-100);
+		
+		this.back = new Button(container, "./Main_Menu", Color.white, false);
+		this.back.setLocation(100, container.getHeight()-50);
 		
 	}
 
@@ -51,15 +61,34 @@ public class GameOver extends BasicGameState{
 		// TODO Auto-generated method stub
 		// draws the background image
 		this.background.draw(0,0,container.getWidth(),container.getHeight());
-		g.setColor(Color.white);
 		
 		// sets the color for the rectangle
 		g.setColor(new Color(0,0,0,0.4f));
 		g.fill(this.box);
 		
-		font.drawString(100,100,"Game Over!");
+		g.setColor(Color.white);
+		
+		font.drawString(100,100,"Graduation!");
+		
+		g.drawString("Name: ",100,175);
+		g.drawString(Game_Controller.player.getName(),400,175);
+		
+		g.drawString("Final GPA: ",100,225);
+		g.drawString(String.format("%.2f",Game_Controller.player.finalGpa()),400,225);
+		
+		this.back.render(container, g);
 	}
 
+	public void mousePressed(int button, int x, int y) {
+		if (button == 0) { // if the left mouse button is pressed
+			if (back.ButtonPressed(x, y)) {
+				
+				game.enterState(0, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
+
+			}
+		}
+	}
+	
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
